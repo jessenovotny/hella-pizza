c9bb1ca26e7b8c41523fee7d5c62f6743d36b50f
Jesse Novotny <jessenovotny@gmail.com>
2017-03-22 12:35:51 -0700

    updated root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+	root to: "pizzas#index"
   resources :toppings
   resources :pizzas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
