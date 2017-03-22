87783d2c1eba5014b1ea437267038f178a8de6dd
Jesse Novotny <jessenovotny@gmail.com>
2017-03-22 12:14:35 -0700

    refactored add_toppings to pizza model. Will add check_box_collection to new/edit form for adding ingredients

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,4 +1,16 @@
 class Pizza < ApplicationRecord
 	has_many :pizza_toppings
 	has_many :toppings, through: :pizza_toppings
+
+	def add_toppings pizza_params
+		if toppings = pizza_params["description"]
+      toppings = toppings.gsub(',', '').gsub('and ', '').split(' ')
+      self.toppings.clear
+      toppings.each do |topping|
+        topping = Topping.find_or_create_by(name: topping)
+        self.toppings << topping unless self.toppings.include?(topping)
+      end
+    end
+    self
+	end
 end
